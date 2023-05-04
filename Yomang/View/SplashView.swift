147658103dce68de062bb03d9951a9369b92a1f8
Jose <dbswkgus99@snu.ac.kr>
2023-05-04 16:19:47 +0900

    [CHORE] START

---
+++ Yomang/View/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  Yomang
+//
+//  Created by jose Yun on 2023/05/04.
+// 
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
---
+++ Yomang/ViewModel/MatchingCodeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MatchingCodeViewModel.swift
+//  Yomang
+//
+//  Created by jose Yun on 2023/05/04.
+//
+
+import Foundation
