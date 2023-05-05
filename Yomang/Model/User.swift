dc18fd0629632671f1e3d4509bb1795a3358f676
khnueng@naver.com <khnueng@naver.com>
2023-05-05 19:41:33 +0900

    [REFACTOR][#7] User Model 변수 형태 통일

--- Yomang/Model/User.swift
+++ Yomang/Model/User.swift
@@ -11,13 +11,13 @@ import Foundation
 struct User{
     // 개인 정보
     var userID: String
-    var partnerID: String?
+    var partnerID: String
 
     // 파트너와 연결되어 있음
     var connected: Bool
 
     // 본인이 설정한 이미지
-    var imageURL: String?
+    var imageURL: String
     
     // 파트너와 연결되어 있는지 확인
     func isConnected() -> Bool {
