a56df2529e985eea4ba5de2a21575583fe3346c8
khnueng@naver.com <khnueng@naver.com>
2023-05-05 19:53:29 +0900

    [REFACTOR][#7] User Model에서 불필요한 isConnected() 삭제

--- Yomang/Model/User.swift
+++ Yomang/Model/User.swift
@@ -10,22 +10,12 @@ import Foundation
 
 struct User{
     // 개인 정보
-    var userID: String
-    var partnerID: String
+    var userID: String = UserDefaults.standard.string(forKey: "userID") ?? ""
+    var partnerID: String = UserDefaults.standard.string(forKey: "partnerID") ?? ""
 
     // 파트너와 연결되어 있음
-    var connected: Bool
+    var connected: Bool = UserDefaults.standard.bool(forKey: "connected")
 
     // 본인이 설정한 이미지
-    var imageURL: String
-    
-    // 파트너와 연결되어 있는지 확인
-    func isConnected() -> Bool {
-        if connected {
-            return true
-        } else {
-            return false
-        }
-    }
-    
+    var imageURL: String = UserDefaults.standard.string(forKey: "imageURL") ?? ""
 }
