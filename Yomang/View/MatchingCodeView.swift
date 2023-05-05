0c85490f5185565ffd0d1575420cdb27ab857250
henry.choi <oreobro@naver.com>
2023-05-05 17:39:05 +0900

    [GUI][#5] First Commit

---
+++ Yomang/View/MatchingCodeView.swift
@@ -0,0 +1,20 @@
+//
+//  MatchingCodeView.swift
+//  Yomang
+//
+//  Created by 최민규 on 2023/05/05.
+//
+
+import SwiftUI
+
+struct MatchingCodeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MatchingCodeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MatchingCodeView()
+    }
+}
