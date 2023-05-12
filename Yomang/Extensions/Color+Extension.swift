12d5aea72299f94e1431a1a06ce5f74fcef28495
Dayeon Moon <0217dayun@naver.com>
2023-05-12 11:42:46 +0900

    [HOTFIX] Color+Extension 중복 선언 삭제

--- Yomang/Extensions/Color+Extension.swift
+++ Yomang/Extensions/Color+Extension.swift
@@ -9,14 +9,11 @@ import Foundation
 import SwiftUI
 
 extension Color {
-//    public static let Color: Color = Color(UIColor())
     init(hex: UInt, alpha: Double = 1.0) {
             let red = Double((hex >> 16) & 0xff) / 255.0
             let green = Double((hex >> 8) & 0xff) / 255.0
             let blue = Double(hex & 0xff) / 255.0
             self.init(red: red, green: green, blue: blue, opacity: alpha)
     }
-    
     static let main500 = Color(hex: 0x7638F9)
-    
 }
--- Yomang/View/MainFlow/ImageMarkUpView.swift
+++ Yomang/View/MainFlow/ImageMarkUpView.swift
@@ -208,17 +208,6 @@ struct CustomSliderView: View {
     }
 }
 
-
-
-extension Color {
-    init(hex: Int, alpha: Double = 1.0) {
-        let red = Double((hex >> 16) & 0xff) / 255.0
-        let green = Double((hex >> 8) & 0xff) / 255.0
-        let blue = Double(hex & 0xff) / 255.0
-        self.init(red: red, green: green, blue: blue, opacity: alpha)
-    }
-}
-
 extension View{
     @ViewBuilder
     func iOSPopover<Content: View>(isPresented: Binding<Bool>, arrowDirection: UIPopoverArrowDirection, @ViewBuilder content: @escaping() -> Content) -> some View {
