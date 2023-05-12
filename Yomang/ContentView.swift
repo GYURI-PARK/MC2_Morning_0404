2b2d40ac6e614dc9fd8e57d838d9f29de3305dc8
Dayeon Moon <0217dayun@naver.com>
2023-05-12 09:18:00 +0900

    [FIX] package build 오류 해결, 없는 파일 호출 삭제

--- Yomang/ContentView.swift
+++ Yomang/ContentView.swift
@@ -13,26 +13,6 @@ struct ContentView: View {
     @State private var connected: Bool = false
     
     var body: some View {
-        if viewModel.userSession == nil {
-            MatchingTest(user: User(uuid: "", userId: "", isConnected: false))
-        } else {
-            if let user = viewModel.user {
-                if !connected {
-                    MatchingTest(user: user)
-                        .onChange(of: user.isConnected) { _ in
-                            connected = true
-                        }
-                } else {
-                    liveMatchingTest(user: user)
-                        .onAppear() {
-                            //signOut 하고싶을 때 사용하기
-//                            viewModel.signOut()
-                        }
-                }
-            }
-//            else {
-//                Text("업성")
-//            }
-        }
+        Text("")
     }
 }
